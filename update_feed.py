1702eb5df1e24e7487fad9a8efa95d2c0edbd2e1
valneken <vpetrus@gmail.com>
2023-12-04 10:05:15 +0100

    yaya

--- update_feed.py
+++ update_feed.py
@@ -1,6 +1,7 @@
 import feedparser
 import json
 import requests
+from datetime import datetime
 
 RSS_FEED_URL = 'https://www.eurojust.europa.eu/rss/press-releases.xml'
 JSON_FILE_PATH = 'rss_data.json'
@@ -9,8 +10,14 @@ def fetch_and_save_feed():
     feed = feedparser.parse(RSS_FEED_URL)
     entries = feed.entries
 
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    data_with_timestamp = {
+        'timestamp': timestamp,
+        'entries': entries
+    }
+
     with open(JSON_FILE_PATH, 'w') as json_file:
-        json.dump(entries, json_file, indent=2)
+        json.dump(data_with_timestamp, json_file, indent=2)
 
 if __name__ == '__main__':
     fetch_and_save_feed()
